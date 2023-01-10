29f2e1ce67960d5022b2d3516e65b266b2f37b88
ncardozo <n.cardozo@uniandes.edu.co>
2023-01-10 18:24:50 -0500

    update graph script
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- semana2/tutoriales/graph.py
+++ semana2/tutoriales/graph.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from bandit import *
 
-# get data
+# x axis data
 x = np.linspace(0, 1000, 1000)
 
-# plot
+# plot definition
 fig, ax = plt.subplots()
 plt.title("Bandit average reward")
 plt.xlabel("Iterations")
 plt.ylabel("Average reward")
-#ax.set(xlim=(0, 1000), xticks=np.arange(1, 1000), 
-#       ylim=(0, 3), yticks=np.arange(0, 3))
 
-def plot_bandits(bandits=1):
+# y axis data definition and graph definition 
+def plot_bandits(bandits=1, cum = 'c'):
     for i in range(1, bandits+1):
         b = Bandit()
-        y = np.array(b.run())
-        ax.plot(x, y, linewidth=2.0, label=f'bandit {i}')
+        y = np.array(b.run(cum))
+        ax.plot(x, y, linewidth=1.0, label=f'bandit {i}')
     ax.legend()
     plt.show()
 
+#example: plot with 3 bandits
+#remove the coment for the case you want to observe
 plot_bandits(3)
+#plot_bandits(3, 't')
